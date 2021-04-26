be7d8520e123fb8e94c77cecc5370bcf3257d68b
陈志谦 <761208@nd.com>
2021-04-26 21:09:20 +0800

    增加gree的polyfill

---
+++ install.sh
@@ -0,0 +1,67 @@
+cd /data
+#### install node and npm
+#arm71
+wget https://npm.taobao.org/mirrors/node/v14.16.1/node-v14.16.1-linux-armv7l.tar.xz
+tar -xvf node-v14.16.1-linux-armv7l.tar.xz
+mv node-v14.16.1-linux-armv7l node-v14.16.1
+
+#x64
+#wget https://npm.taobao.org/mirrors/node/v14.16.1/node-v14.16.1-linux-x64.tar.xz
+#tar -xvf node-v14.16.1-linux-x64.tar.xz
+#mv node-v14.16.1-linux-x64 node-v14.16.1
+
+
+
+
+
+#在/etc/environment 加入 :/data/node-v14.16.1/bin
+
+#### config
+# set PATH
+export PATH=$PATH:/data/node-v14.16.1/bin
+# set npm use root
+npm config -g set unsafe-perm=true
+# set npm use taobao mirror
+npm config set registry https://registry.npm.taobao.org
+
+
+
+
+
+#### install mqtt-server
+cd /data
+#git clone http://761208@172.24.132.179/a/app-cpp/local-iot mqtt-server -b mqtt-server
+git clone http://761208@gerrit.sdp.nd/a/app-cpp/local-iot mqtt-server -b mqtt-server
+
+cd /data/mqtt-server
+npm install
+
+
+#### install cmd gateway
+cd /data
+#ssh download
+#git clone ssh://761208@gerrit.sdp.nd:29418/app-cpp/local-iot cmd-gateway -b cmd-gateway
+
+#http download
+git clone http://761208@gerrit.sdp.nd/a/app-cpp/local-iot cmd-gateway -b cmd-gateway
+
+
+
+cd /data/cmd-gateway
+npm install
+cd /data/cmd-gateway/data
+npm install
+
+#cd /data/cmd-gateway
+#npm start
+
+
+# auto start
+# see https://blog.csdn.net/sayyy/article/details/79276575
+cp cmd-gateway.service /lib/systemd/system/cmd-gateway.service
+systemctl enable cmd-gateway
+systemctl list-unit-files|grep cmd-gateway
+
+cp mqtt-server.service /lib/systemd/system/mqtt-server.service
+systemctl enable mqtt-server
+systemctl list-unit-files|grep mqtt-server
--- src/polyfill/index.js
+++ src/polyfill/index.js
@@ -1,8 +1,18 @@
 const fs = require("fs");
 const path = require("path");
 
-// 方法五 copyFile 
+
 const fromFileName = path.resolve(__dirname, "10-mqtt.js");
 const toFileName = path.resolve(__dirname, "../../node_modules/@node-red/nodes/core/network/10-mqtt.js");
 
-fs.copyFileSync(fromFileName, toFileName);
+const files = new Map();
+files.set(  path.resolve(__dirname, "node_modules/@node-red/nodes/core/network/10-mqtt.js"), 
+            path.resolve(__dirname, "../../node_modules/@node-red/nodes/core/network/10-mqtt.js"));
+files.set(  path.resolve(__dirname, "data/node_modules/gree-hvac-client/lib/client.js"), 
+            path.resolve(__dirname, "../../data/node_modules/gree-hvac-client/lib/client.js"));
+
+
+files.forEach((toFileName, fromFileName) => {
+  console.log(fromFileName + " = " + toFileName);
+  fs.copyFileSync(fromFileName, toFileName);
+}, files);
