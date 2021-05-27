798f0859285c7af8b309a801252c08bdd4ef4576
陈志谦 <761208@nd.com>
2021-05-27 14:56:37 +0800

    增加websocket连接

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const aedes = require('aedes')()
+
+// tcp server
 const server = require('net').createServer(aedes.handle)
 const port = 1883
 
@@ -8,10 +10,27 @@ server.listen(port, function () {
 
 
 server.addListener("connection", (socket) => {
-    console.log('mqtt server on connection ');
+    console.log('mqtt tcp on connection ');
 })
 
 
+// websocket server
+const wsport = 1882
+const httpServer = require('http').createServer()
+const wsStream = require('websocket-stream')
+
+const wsServer = wsStream.createServer({ server: httpServer }, aedes.handle)
+
+httpServer.listen(wsport, function () {
+  console.log('websocket server listening on port ', wsport)
+})
+
+wsServer.on("connection", (socket) => {
+    console.log('mqtt websocket on connection ');
+})
+
+
+// mqtt event
 aedes.on("client", (client) => {
     console.log('on client event ');
 })
